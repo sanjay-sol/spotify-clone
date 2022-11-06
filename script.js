544832813bc3eece4b6f3e46ced21739f8d11843
Sanjay Sirangi <sanjaysirangi@gmail.com>
2022-11-06 21:04:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+console.log("welcome to Spotify");
+let songIndex =0;
+
+let audioElement = new Audio('./songs/1.mp3');
+
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+let songs = [
+    {songName: "Heat Waves",filepath:"songs/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Scared to Leave",filePath:"songs/2.mp3",coverPath:"covers/2.jpg"},
+    {songName: "Alone Again",filePath:"songs/3.mp3",coverPath:"covers/3.jpg"},
+    {songName: "Enemy",filePath:"songs/4.mp3",coverPath:"covers/4.jpg"},
+    {songName: "Heartless",filePath:"songs/5.mp3",coverPath:"covers/5.jpg"},
+    {songName: "Blinding Lights",filePath:"songs/6.mp3",coverPath:"covers/6.jpg"},
+    {songName: "Call out My name",filePath:"songs/7.mp3",coverPath:"covers/7.jpg"},
+    {songName: "Starboy-check",filePath:"songs/8.mp3",coverPath:"covers/8.jpg"},
+    {songName: "Let me Loveyou",filePath:"songs/9.mp3",coverPath:"covers/9.jpg"},
+     {songName: "Starboy",filePath:"songs/10.mp3",coverPath:"covers/10.jpg"},
+]
+// songs.forEach((element,i)=>{
+//     console.log(element,i);
+//     element.getElementsByTagName("img")[0].src = songs[i].filePath;
+
+// })
+songItems.forEach((element,i)=>{
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+
+})
+//handling play/pause click
+     masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+
+})
+//Listen to events
+audioElement.addEventListener('timeupdate',()=>{
+console.log('timeupdate');
+//update seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;  
+})
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = myProgressBar.value*audioElement.duration/100;
+})
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+         makeAllPlays();
+        index = parseInt(e.target.id);
+         e.target.classList.remove('fa-play-circle');
+         e.target.classList.add('fa-pause-circle');
+         audioElement.src = `songs/${index+1}.mp3`;
+         audioElement.currentTime = 0;
+         audioElement.play();
+         masterPlay.classList.remove('fa-play-circle');
+         masterPlay.classList.add('fa-pause-circle');
+     })
+ })
+ 
